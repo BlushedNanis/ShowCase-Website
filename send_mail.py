31813fac7f1f9c4ce4a0ed7edfa80b98da4917af
BlushedNanis <luis_danieltg@outlook.com>
2024-05-27 17:13:23 -0400

    Added comments

--- send_mail.py
+++ send_mail.py
@@ -6,9 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def send_email(message):
-    mail_addr = "blushedn@gmail.com"
+def send_email(message:str, mail_addr=getenv("email"), mail_pass=getenv("pypass")):
+    """Sends an email with the given message to the given email in .env
+
+    Args:
+        message (str): E-mail message (body)
+        mail_addr (_type_, optional): E-mail address. Defaults to getenv("email").
+        mail_pass (_type_, optional): E-mail password. Defaults to getenv("pypass").
+    """
     with SMTP_SSL("smtp.gmail.com", 
                   context=create_default_context()) as server:
-        server.login(mail_addr, getenv("pypass"))
+        server.login(mail_addr, mail_pass)
         server.sendmail(mail_addr, mail_addr, message)
